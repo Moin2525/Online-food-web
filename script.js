dd71fcd1fae23c077d79f3495a3e523a04fd5ab0
Muhammad Moin <132272975+Moin2525@users.noreply.github.com>
2023-08-26 20:10:25 +0500

    finally

---
+++ script.js
@@ -0,0 +1,36 @@
+//alert("Use good explorer");
+
+//prompt("What's your name?","Guest");
+
+/*confirm("Are you sure you wanna delete this post?");
+
+if(confirm("Are you sure you wanna delete this post?")){
+console.log("It is deleted successfully");
+}
+else{
+    console.log("It is not deleted");
+}*/
+
+console.log(new Date());
+
+setInterval(date, 1000);
+
+function date() {
+  time.innerHTML = new Date();
+}
+
+
+/*const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});*/
